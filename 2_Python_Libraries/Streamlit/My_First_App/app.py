dd23bf74402b0598fba61a61cba96a62d9c6bdbf
Akshay Bhujbal <79648471+AkshayBhujbal1995@users.noreply.github.com>
2025-06-12 11:59:56 +0530

    Add files via upload

---
+++ 2_Python_Libraries/Streamlit/My_First_App/app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.title("👋 Welcome to my app")
+st.subheader("This is my first Streamlit app with a surprise! 🎉")
+
+if st.button("Click me for a secret 🎁"):
+    st.success("🎊 You’re awesome! Keep learning and coding 🚀")
